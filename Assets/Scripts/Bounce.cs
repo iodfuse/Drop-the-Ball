9fca3f42e5cef9aa3fa3cd8158a07003c0d46330
Chris <zitoon@outlook.com>
2026-08-14 23:19:18 -0500

    Partial progress on rebuilding the game from scratch in order to get dynamic shadows working

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bounce : MonoBehaviour {
+
+    public float bounceSpeed;
+    public AudioSource bounceSound;
+
+    private void OnCollisionEnter(Collision collision) {
+//        Vector3 direction = collision.transform.position - transform.position;
+//        float impact = Vector3.Project(collision.relativeVelocity, direction).magnitude;
+        if (collision.impulse.magnitude >= bounceSpeed && SaveGame.save.GetSoundEnabled()) {
+            bounceSound.volume = SaveGame.save.GetVolume();
+            bounceSound.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    public MenuSystem menu;
+    public Collider ball;
+    public AudioSource explode;
+
+    void OnTriggerEnter(Collider other) {
+        if (other == ball) {
+            menu.WinGame();
+            if (SaveGame.save.GetSoundEnabled()) {
+                explode.volume = SaveGame.save.GetVolume();
+                explode.Play();
+            }
+        }
+    }
+}
+
+// This script is only to handle the goals trigger and call the menu system.
